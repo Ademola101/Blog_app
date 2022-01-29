f4caf3b4c9ccffa973f2ed12e3ee931729167487
Ademola <ademolaogunmokun@yahoo.com>
2022-01-29 09:29:13 +0100

    show controller and and show.erb.html

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-
-  get '/articles', to: 'articles#index'
+  resources :articles
   
   devise_scope :user do
     get 'users/sign_out' => "devise/sessions#destroy"
