671422018e211afd6004d3320adcf4c206da51c8
Ademola <ademolaogunmokun@yahoo.com>
2022-01-31 15:55:59 +0100

    create comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,9 @@
 class CommentsController < ApplicationController
+
+  def create
+    @article = Article.find(params[:id])
+    @comment = @article.comment.create(comment_params)
+    redirect_to article_path(@article)
+    
+  end
 end
