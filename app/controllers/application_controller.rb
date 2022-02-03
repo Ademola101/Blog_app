12fe8ff1af6feae893e472aba1096cd9a9e76050
Ademola <ademolaogunmokun@yahoo.com>
2022-02-03 08:01:50 +0100

    add name to reg from

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
   before_action :set_theme
+  before_action :configure_permitted_paramters, if: :devise_controller?
 
   def set_theme
     if params[:theme].present?
@@ -8,5 +10,11 @@ class ApplicationController < ActionController::Base
       cookies[:theme] = theme
       redirect_to(request.referrer || root_path)
     end
+  end
+  protected
+
+  def configure_permitted_paramters
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password)}
+    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email, :password, :current_password)}
   end
 end
