984e693125ace16d46328dc82430582eb286de48
Ademola <ademolaogunmokun@yahoo.com>
2022-02-21 01:53:02 +0100

    addvisible

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,11 +1,10 @@
 class Article < ApplicationRecord
+  def to_param
+    "#{id}-#{title.parameterize}"
+  end
   include Visible
   validates :title, presence: true
   validates :body, presence: true, length: { minimum: 10 }
   belongs_to :user
   has_many :comments, dependent: :destroy
-
-  def to_param
-    "#{id}-#{title.parameterize}"
-  end
 end
